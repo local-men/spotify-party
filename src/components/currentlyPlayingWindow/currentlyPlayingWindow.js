4e6fd6b37d18d53b1276f51202c4046e5010ca1e
soldered-snake <kyle.vinc11@gmail.com>
2020-01-02 00:33:07 +1000

    Updated Currenly Playing
    Less glitchy now, still have some lag between song skips.

--- src/components/currentlyPlayingWindow/currentlyPlayingWindow.js
+++ src/components/currentlyPlayingWindow/currentlyPlayingWindow.js
@@ -8,9 +8,10 @@ class CurrentlyPlayingWindowComponent extends React.Component {
         super();
         this.state = {
             token: '',
-            currentUri: ["spotify:track:2lK2CqSAHK6QkdQOvk3vFJ", "spotify:track:7sO5G9EABYOXQKNPNiE9NR"],
+            uriArray: ["spotify:track:2lK2CqSAHK6QkdQOvk3vFJ", "spotify:track:7sO5G9EABYOXQKNPNiE9NR"],
+            songQueue: ["spotify:track:2lK2CqSAHK6QkdQOvk3vFJ", "spotify:track:7sO5G9EABYOXQKNPNiE9NR"],
+            playing: true,
             someBool: false,
-            nextSong: true
         }
     }
 
@@ -26,31 +27,28 @@ class CurrentlyPlayingWindowComponent extends React.Component {
                 <button onClick={this.changeUri}> change between 2 songs. </button>
                 <SpotifyPlayer
                     token={this.state.token}
-                    uris={this.state.currentUri}
+                    uris={this.state.songQueue}
                     autoPlay={false}
+                    play={this.state.playing}
                     name={"Spotify House Party"}
-                    callback={state => this.handleState(state)}/>
+                    callback={(state) => this.handleState(state)}/>
             </div>
         );
     }
 
-    //State change
-    handleState = (state) => {
-        //If next song is true
-        if(this.state.nextSong){
-            this.changeUri();
-            this.setState({nextSong: false})
-        } else{
-            this.setState({nextSong: true})
-        }
+    //State change,
+    handleState = async(state) => {
+        console.log('big poop');
+        //This is working, but there is a flash on the image when re-rendering the state, sometimes audio jumps too...
+        this.setState({songQueue: this.state.uriArray});
     };
 
     //TODO: this works for now.
     changeUri = () => {
         this.state.someBool ?
-            this.setState({currentUri: ["spotify:track:2lK2CqSAHK6QkdQOvk3vFJ", "spotify:track:7sO5G9EABYOXQKNPNiE9NR"], someBool: false})
+            this.setState({uriArray: ["spotify:track:2lK2CqSAHK6QkdQOvk3vFJ", "spotify:track:7sO5G9EABYOXQKNPNiE9NR"], someBool: false})
                 :
-            this.setState({currentUri: ['spotify:track:7sO5G9EABYOXQKNPNiE9NR', "spotify:track:2lK2CqSAHK6QkdQOvk3vFJ"], someBool: true});
+            this.setState({uriArray: ["spotify:track:2e3g8go386Zn6EyIz60Ci9", "spotify:track:2lK2CqSAHK6QkdQOvk3vFJ"], someBool: true});
     }
 }
 
